3a770fdebd3036a7b7156c700c2854afd442700f
AurelijaZuba <auryte00@hotmail.com>
2019-03-12 09:45:35 +0000

    initial commit

---
+++ src/test/java/com/codurance/StringCalculatorShould.java
@@ -0,0 +1,16 @@
+package com.codurance;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class StringCalculatorShould
+{
+
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+
+        assertTrue( true );
+    }
+}
