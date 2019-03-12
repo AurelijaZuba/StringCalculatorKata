e52ccc6d6dd06ae1bab239eb1e20e03c72ceb86f
AurelijaZuba <auryte00@hotmail.com>
2019-03-12 12:18:39 +0000

    working on sum numbers

--- src/main/java/com/codurance/StringCalculator.java
+++ src/main/java/com/codurance/StringCalculator.java
@@ -1,9 +1,27 @@
 package com.codurance;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringCalculator
 {
-    public static int add( String numbers) {
+    public static int add(String numbers) {
+        String[] splitNumbers = numbers.split(",");
+
         if(numbers == "") return 0;
+
+        List<Integer> storedNumbers = new ArrayList<>();
+       for(int i = 0; i < splitNumbers.length; i++)
+        {
+            storedNumbers.add(Integer.parseInt(splitNumbers[i]));
+        }
+       int sumNumber = 0;
+        for(int i = 0; i < storedNumbers.size(); i++)
+        {
+            sumNumber += storedNumbers[i];
+        }
+
         return Integer.parseInt(numbers);
+        //return storedNumbers;
     }
 }
