b25a1b8ad5dc4ee9748b6a043e948789297efcff
Gabriele Lippi <gabriele@lippi.net>
2019-03-12 12:42:00 +0100

    Make third test pass

--- src/main/java/com/codurance/StringCalculator.java
+++ src/main/java/com/codurance/StringCalculator.java
@@ -3,8 +3,7 @@ package com.codurance;
 public class StringCalculator
 {
     public static int add( String numbers) {
-        if(numbers == "")
-        return 0;
-        return 1;
+        if(numbers == "") return 0;
+        return Integer.parseInt(numbers);
     }
 }
